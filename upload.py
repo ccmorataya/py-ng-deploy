db1b44608027f9ac203ecff5eeb33985cba6aba7
Cristian Morataya <cris.morataya@gmail.com>
2019-05-21 16:31:36 -0600

    Validate if backup folder exist if not create it

--- upload.py
+++ upload.py
@@ -9,14 +9,18 @@ with open('config.json') as json_config:
     json_data = json.load(json_config)
 
 srv = pysftp.Connection(host=json_data['host'], username=json_data['username'], password=json_data['password'], cnopts=cnopts)
+backup_path = json_data['angular_project_root'] + json_data['local_dir'] + json_data['backup_folder']
+base_path = json_data['angular_project_root'] + json_data['local_dir'] + json_data['base']
 
-print('Backup to '+ os.path.abspath(json_data['angular_project_root'] + json_data['local_dir']) + json_data['backup_folder'])
+print('Backup to '+ backup_path)
 srv.chdir(json_data['remote_dir'])
-# TODO-CM: validate if the backup_folder exists if not create it
-srv.get_r('.', json_data['angular_project_root'] + json_data['local_dir'] + json_data['backup_folder'], preserve_mtime=True)
+if not os.path.isdir(backup_path):
+    print('Backup path doesn\'t exists, creating')
+    os.mkdir(backup_path)
+srv.get_r('.', backup_path, preserve_mtime=True)
 print('Done :D')
 print('Uploading to ' + json_data['remote_dir'])
-srv.put_r(json_data['angular_project_root'] + json_data['local_dir'] + json_data['base'], json_data['remote_dir'], preserve_mtime=True)
+srv.put_r(base_path, json_data['remote_dir'], preserve_mtime=True)
 print('Done :D')
 
 srv.close()
