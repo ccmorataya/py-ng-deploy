61ab216ba396cf232cc12a269d78a7e767cbb3b6
Cristian Morataya <cris.morataya@gmail.com>
2019-05-21 16:09:06 -0600

    Allows to call ng-py-deploy from regular terminal

--- ng-git-build.py
+++ ng-git-build.py
@@ -12,9 +12,12 @@ if len(sys.argv) > 1 and sys.argv[1] == 'prod':
     ngBuild.append('--prod')
 
 with open('config.json') as json_config:
-    data = json.load(json_config)
+    json_data = json.load(json_config)
 
+ng_py_deploy_root = os.getcwd()
+os.chdir(json_data['angular_project_root'])
 result = sp.run(ngBuild)
+os.chdir(ng_py_deploy_root)
 
 if result.returncode == 0:
     # CM: get the hash
@@ -26,13 +29,13 @@ if result.returncode == 0:
         sha = sha[objSlice]
 
         # CM: alter index.html
-        with open(data['index_path'], 'r') as file:
+        with open(json_data['angular_project_root'] + json_data['local_dir'] + json_data['base'] + '/index.html', 'r') as file:
             data = file.readlines()
 
         # TODO-CM: add branch name
         data[4] = '  <title>:: Commit:' + sha + '</title>\n'    # project title
 
-        with open(data['index_path'], 'w') as file:
+        with open(json_data['angular_project_root'] + json_data['local_dir'] + json_data['base'] + '/index.html', 'w') as file:
             file.writelines( data )
         print('The hash is ' + sha)
 
@@ -40,13 +43,10 @@ if result.returncode == 0:
     if os.name == 'nt':
         spCallParams.append('bash.exe')
         spCallParams.append('-c')
-    # if os.name == 'posix':
-    #     spCallParams.append(f'python {data["upload_py_path"]}')
     spCallParams.append('python')
-    spCallParams.append(f'{data["upload_py_path"]}')    # '' = optional: source ~/.rcfile;  # ... python /relative/path/to/pruebas.py
+    spCallParams.append('upload.py')
 
     result = sp.call(spCallParams, stderr=sp.DEVNULL)
-    # result = sp.call(spCallParams)
     if result == 0:
         print('Process complete :D')
     else:
--- upload.py
+++ upload.py
@@ -6,17 +6,17 @@ cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
 
 with open('config.json') as json_config:
-    data = json.load(json_config)
+    json_data = json.load(json_config)
 
-srv = pysftp.Connection(host=data['host'], username=data['username'], password=data['password'], cnopts=cnopts)
+srv = pysftp.Connection(host=json_data['host'], username=json_data['username'], password=json_data['password'], cnopts=cnopts)
 
-print('Backup to '+ os.path.abspath(data['local_dir']) + data['backup_folder'])
-srv.chdir(data['remote_dir'])
-# TODO-CM: validate if the backup_folder exists
-srv.get_r('.', data['local_dir'] + data['backup_folder'], preserve_mtime=True)
+print('Backup to '+ os.path.abspath(json_data['angular_project_root'] + json_data['local_dir']) + json_data['backup_folder'])
+srv.chdir(json_data['remote_dir'])
+# TODO-CM: validate if the backup_folder exists if not create it
+srv.get_r('.', json_data['angular_project_root'] + json_data['local_dir'] + json_data['backup_folder'], preserve_mtime=True)
 print('Done :D')
-print('Uploading to ' + data['remote_dir'])
-srv.put_r(data['local_dir'] + data['base'], data['remote_dir'], preserve_mtime=True)
+print('Uploading to ' + json_data['remote_dir'])
+srv.put_r(json_data['angular_project_root'] + json_data['local_dir'] + json_data['base'], json_data['remote_dir'], preserve_mtime=True)
 print('Done :D')
 
 srv.close()
