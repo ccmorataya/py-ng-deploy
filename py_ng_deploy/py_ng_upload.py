a6e4fcfe90f3cf9b120573dc19fa92c7848de15f
Cristian Morataya <cris.morataya@gmail.com>
2019-06-12 09:54:33 -0600

    Add Port configuration with default value

--- py_ng_deploy/py_ng_upload.py
+++ py_ng_deploy/py_ng_upload.py
@@ -22,6 +22,7 @@ def host_key_check(environment):
         with pysftp.Connection(host=config[environment]['Host'],
                                username=config[environment]['Username'],
                                password=config[environment]['Password'],
+                               port=config[environment]['Port'],
                                cnopts=cnopts) as sftp:
             if hostkeys is not None:
                 print(
@@ -35,6 +36,7 @@ def host_key_check(environment):
     return pysftp.Connection(host=config[environment]['Host'],
                              username=config[environment]['Username'],
                              password=config[environment]['Password'],
+                             port=config[environment]['Port'],
                              cnopts=cnopts)
 
 
